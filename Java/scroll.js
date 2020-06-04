1e08bfa513cd45b6abbb9ecdc66952e1c267aac0
elliottmcdonald1998 <65764929+elliottmcdonald1998@users.noreply.github.com>
2020-06-04 12:42:52 +0100

    menu button
    menu

--- Java/scroll.js
+++ Java/scroll.js
@@ -105,3 +105,13 @@ function prevFunction() {
 
 
 }
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
